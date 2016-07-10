a7fd43cb6ad0c2cd509bdbedcd4875e1fadbfc58
logeshr63 <logeshr63@gmail.com>
2016-07-10 12:48:45 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -1,34 +1,25 @@
-# four
-package demo;
-
-import com.mtsystems.coot.String8;
-
-import java.util.Scanner;
-
-public class sort {
-	public static void main(String[] args) {
-		String8 s = new String8(5, 20);
-		String8 t = new String8(20);
-		System.out.print("\nEnter any five strings : ");
-		for(int i = 0; i < 5; i++) {
-			s.shift(i, 0).copyFrom(STDIN_SCANNER.next());
-		}
-
-		for(int i = 1; i < 5; i++) {
-			for(int j = 1; j < 5; j++) {
-				if(s.shift(j - 1, 0).compareTo(s.shift(j, 0)) > 0) {
-					t.copyFrom(s.shift(j - 1, 0));
-					s.shift(j - 1, 0).copyFrom(s.shift(j, 0));
-					s.shift(j, 0).copyFrom(t);
-				}
-			}
-		}
-
-		System.out.print("\nStrings in order are : ");
-		for(int i = 0; i < 5; i++) {
-			System.out.print("\n" + s.shift(i, 0));
-		}
-	}
-
-	public final static Scanner STDIN_SCANNER = new Scanner(System.in);
+#include<stdio.h>
+#include<string.h>
+ 
+int main() {
+   char s[5][20], t[20];
+   int i, j;
+   printf("\nEnter any five strings : ");
+   for (i = 0; i < 5; i++)
+      scanf("%s", s[i]);
+ 
+   for (i = 1; i < 5; i++) {
+      for (j = 1; j < 5; j++) {
+         if (strcmp(s[j - 1], s[j]) > 0) {
+            strcpy(t, s[j - 1]);
+            strcpy(s[j - 1], s[j]);
+            strcpy(s[j], t);
+         }
+      }
+   }
+ 
+   printf("\nStrings in order are : ");
+   for (i = 0; i < 5; i++)
+      printf("\n%s", s[i]);
+ 
 }
